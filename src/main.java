d4f4d2f20fa500912b272a60be7ca7e8d33c24b6
Skyler <bluesky900@att.net>
2018-11-30 13:43:33 -0800

    Post and status additions
    - add/remove posts now working
    - can now update user status
    - added user settings table to the database

--- src/main.java
+++ src/main.java
@@ -7,10 +7,11 @@ public class main {
 		DBInterface connection = new DBInterface();
 		//System.out.println(connection.login("\'testMail1@gmail.com\'", "password"));
 		//connection.register("\'registerTest\'", "\'testword\'", "\'Peter\'", "\'Vander\'", 52);
-		//connection.searchData("Users", "FirstName, LastName, Password, Email", "FirstName = \'Peter\' AND LastName = \'Vander\'");
-		System.out.println(connection.addFriend(12, 11));
-		connection.removeFriend(12, 11);
-		System.out.println(connection.addFriend(12, 11));
+		connection.updateStatus(1, "\'I am testing this status\'");
+		connection.searchData("Users", "FirstName, LastName, Password, Email, Status", "Email = \'testMail1@gmail.com\'");
+		//System.out.println(connection.addPost(1, "\'Test post\'", "\'This is a test of the posting system\'"));
+		//connection.removePost(1, 4);
+		//System.out.println(connection.addPost(12, 11));
 		connection.logout();
 	}
 
