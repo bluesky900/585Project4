48ba7f4466315b6b277494733a2d65d4a42c540d
Mohammadreza Shahbazi <mshabazi68@gmail.com>
2018-12-01 15:13:56 -0800

    Add files via upload

--- src/main.java
+++ src/main.java
@@ -1,7 +1,24 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 import java.sql.SQLException;
 
-public class main {
-
+public class main extends Application {
+    public void start (Stage stage) throws Exception {
+        try {  Parent root = (Parent) FXMLLoader.load(getClass().getResource("login.fxml"));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.setTitle("Facebook Lite");
+            stage.show();
+            stage.setResizable(false);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 	public static void main(String[] args) throws SQLException {
 
         DBInterface connection = new DBInterface();
@@ -13,6 +30,7 @@ public class main {
         //connection.removePost(1, 4);
         //System.out.println(connection.addPost(12, 11));
         connection.logout();
+        launch(args);
 	}
 
 }
